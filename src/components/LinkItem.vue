2b9661be6e3ebffa6c47eca58af30c6910e35248
Maxime Pelletier <pelletiermaxime@gmail.com>
2017-10-01 14:59:18 -0400

    Fix voting by passing variables

--- src/components/LinkItem.vue
+++ src/components/LinkItem.vue
@@ -59,8 +59,18 @@
         })
       },
       updateStoreAfterVote (store, createVote, linkId) {
+        const page = parseInt(this.$route.params.page, 10)
+        const isNewPage = this.$route.path.includes('new')
+        const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0
+        const first = isNewPage ? LINKS_PER_PAGE : 100
+        const orderBy = isNewPage ? 'createdAt_DESC' : null
         const data = store.readQuery({
-          query: ALL_LINKS_QUERY
+          query: ALL_LINKS_QUERY,
+          variables: {
+            first: first,
+            skip: skip,
+            orderBy: orderBy
+          }
         })
 
         const votedLink = data.allLinks.find(link => link.id === linkId)
